c934e94cf093bdf3c85970c1f961996e12d6e67b
MadhuByreddi116 <madhubyreddi116@gmail.com>
2024-08-10 16:13:51 +0530

    initial commit

---
+++ SelectionSort/Solution.java
@@ -0,0 +1,21 @@
+import java.util.* ;
+import java.io.*; 
+public class Solution {
+	public static void selectionSort(int arr[], int n) {
+		int temp;
+		for (int i =0;i<n-1;i++)
+		{
+			for (int j=0;j<n-1;j++)
+			{
+				if(arr[j]>arr[j+1])
+				{
+                temp = arr[j];
+				arr[j]=arr[j+1];
+				arr[j+1]=temp;
+				}
+			}
+		}
+    
+       
+	}
+}
